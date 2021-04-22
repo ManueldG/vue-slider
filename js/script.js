f1aaf1dca25dd0bed4f79b3681db5d62b38e0c3d
Manuel della Gala <manuel.dellagala@gmail.com>
2021-04-22 02:22:29 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -2,7 +2,33 @@ function main(){
     var app = new Vue({
         el: '#app',
         data: {
-          message: 'Hello Vue!'
+          images:[
+            'http://placeimg.com/1200/800/any/sepia',
+            'http://placeimg.com/1200/800/any/grayscale',
+            'https://www.fillmurray.com/1200/800',
+            'https://www.placecage.com/1200/800',
+            'https://placekitten.com/1200/800'
+          ],
+          indexImage:0
+        },
+        methods:{
+            nextImg(){
+                console.log(this.indexImage);
+                this.indexImage= (this.indexImage>=this.images.length-1)? this.indexImage=0 : ++this.indexImage;                
+            },
+            prevImg(){                
+                this.indexImage=(this.indexImage<=0)? this.indexImage=this.images.length-1 : --this.indexImage;                
+            },
+            setPhoto(i){
+                console.log(i);
+                this.indexImage=i;
+            },
+            todo: function(){           
+            setInterval(nextImg,1000);
         }
+            
+        },
+        
       })
 }
+
